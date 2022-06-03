b408542e013eab3d0d948afda2ad31f1bc5206d5
^_^ <1063787103@qq.com>
2022-06-03 15:53:46 +0800

    del

--- TarHelper/Maps.cpp
+++ TarHelper/Maps.cpp
@@ -10,8 +10,6 @@ Maps::Maps(QWidget *parent)
 	: QWidget(parent), qimg(":/Maps/reserve"), imgSizeDivisor(1)
 {	
 	ui.setupUi(this);
-	
-	ui.maps->setPixmap(QPixmap::fromImage(qimg));
 
 	ui.mapArea->installEventFilter(this);	//安装事件过滤器
 
@@ -37,25 +35,21 @@ void Maps::paintEvent(QPaintEvent *event) {
 void Maps::on_reserve_clicked() {
 	anim_slide(0);
 	qimg.load(":/Maps/reserve");
-	ui.maps->setPixmap(QPixmap::fromImage(qimg));
 }
 
 void Maps::on_forest_clicked() {
 	anim_slide(3);
 	qimg.load(":/Maps/forest");
-	ui.maps->setPixmap(QPixmap::fromImage(qimg));
 }
 
 void Maps::on_interchange_clicked() {
 	anim_slide(1);
 	qimg.load(":/Maps/interchange");
-	ui.maps->setPixmap(QPixmap::fromImage(qimg));
 }
 
 void Maps::on_shoreline_clicked() {
 	anim_slide(2);
 	qimg.load(":/Maps/shoreline");
-	ui.maps->setPixmap(QPixmap::fromImage(qimg));
 }
 
 void Maps::anim_slide(int num) {
@@ -70,23 +64,5 @@ void QAbstractScrollArea::wheelEvent(QWheelEvent* event) {
 }
 
 bool Maps::eventFilter(QObject* watched, QEvent* event) {
-	if (watched == ui.mapArea && event->type() == QEvent::Wheel) {
-		QWheelEvent* wheelEvent = static_cast<QWheelEvent*>(event);
-		QRect tmp = ui.maps->geometry();
-		int adjustSize = 500;
-		/*图像缩放*/
-		if (wheelEvent->delta() > 0) {
-			imgSizeDivisor > 1 ? --imgSizeDivisor : NULL;
-			ui.maps->setPixmap(QPixmap::fromImage(
-				qimg.scaled(qimg.width() / imgSizeDivisor, qimg.height() / imgSizeDivisor,
-					Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation)));
-		}
-		else if (wheelEvent->delta() < 0) {
-			++imgSizeDivisor;
-			ui.maps->setPixmap(QPixmap::fromImage(
-				qimg.scaled(qimg.width() / imgSizeDivisor, qimg.height() / imgSizeDivisor,
-					Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation)));
-		}
-	}
 	return true;
 }
